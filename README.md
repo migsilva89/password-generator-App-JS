ae73923e3172e1ef3e8100c2eb8fcddab8ee6fd2
migsilva89 <migdrum@gmail.com>
2022-10-12 16:12:29 +0100

    final commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Password Generator APP - Javascript
+
+![Alt Text](https://gyazo.com/027e4d24514c2a92e9967a1989b2b5bc.gif)
+
+### The challenge was to make a password generator with Javascript where we can:
+
+- Generate a password based on the inclusion options selected;
+- Copy a generated password to a computer clipboard;
+- Get a strength rating for the password;
+
+## Built with
+
+- Semantic HTML5 markup;
+- TailiwndCSS CDN;
+- Javascript;
+
+[Live Url here.](https://password-generator-app-js.vercel.app/).
+
+Tips or advice on how to improve are very welcome, thank you all!
+
+[Linkedin](https://www.linkedin.com/in/miguelmpsilva/) -
+[Twitter](https://twitter.com/Miguelmpsilva) 
--- index.js
+++ index.js
@@ -1,167 +1,164 @@
-let rangeLength = document.getElementById("range-length");  
-let rangeResult = document.getElementById("range-result");
-let generateBtn = document.getElementById("generate-btn");
-let passwordResult = document.getElementById("password-result");
+let rangeLength = document.getElementById('range-length')
+let rangeResult = document.getElementById('range-result')
+let generateBtn = document.getElementById('generate-btn')
+let passwordResult = document.getElementById('password-result')
 let characters = ''
 
-rangeLength.addEventListener("change", getRange)
-generateBtn.addEventListener("click", getInfoForPasswordGenerator)
+rangeLength.addEventListener('change', getRange)
+generateBtn.addEventListener('click', getInfoForPasswordGenerator)
 
 function getRange() {
-    rangeResult.innerHTML = rangeLength.value
-    return rangeLength.value
+  rangeResult.innerHTML = rangeLength.value
+  return rangeLength.value
 }
 
-function getInfoForPasswordGenerator(){
+function getInfoForPasswordGenerator() {
+  var boxNumber = parseInt(getCheckedBoxes())
 
-    var boxNumber = parseInt(getCheckedBoxes())
-
-    //IF IS NUMBER IT GOES TROUHG SWITCH
-    if(!isNaN(boxNumber)){
-        
+  //IF IS NUMBER IT GOES TROUHG SWITCH
+  if (!isNaN(boxNumber)) {
     evaluatePassword()
-    
-        switch (boxNumber) {
-            case 12:
-                characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-                break;
 
-            case 13:
-                characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-                break;
+    switch (boxNumber) {
+      case 12:
+        characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+        break
 
-            case 14:
-                characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+=-/*-'
-                break;
+      case 13:
+        characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+        break
 
-            case 123:
-                characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-                break;
+      case 14:
+        characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+=-/*-'
+        break
 
-            case 124:
-                characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+=-/*-'
-                break;
+      case 123:
+        characters =
+          'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+        break
 
-            case 1234:
-                characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+=-/*-'
-                break;
+      case 124:
+        characters =
+          'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+=-/*-'
+        break
 
-            case 134:
-                characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+=-/*-'
-                break;
+      case 1234:
+        characters =
+          'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+=-/*-'
+        break
 
-            case 23:
-                characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
-                break;
+      case 134:
+        characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+=-/*-'
+        break
 
-            case 24:
-                characters = 'abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+=-/*-'
-                break;
+      case 23:
+        characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
+        break
 
-            case 34:
-                characters = '0123456789!@#$%^&*()_+=-/*-'
-                break;
+      case 24:
+        characters = 'abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+=-/*-'
+        break
 
-            case 1:
-                characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-                break;
+      case 34:
+        characters = '0123456789!@#$%^&*()_+=-/*-'
+        break
 
-            case 2:
-                characters = 'abcdefghijklmnopqrstuvwxyz'
-                break;
+      case 1:
+        characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        break
 
-            case 3:
-                characters = '0123456789'
-                break;
+      case 2:
+        characters = 'abcdefghijklmnopqrstuvwxyz'
+        break
 
-            case 4:
-                characters = '!@#$%^&*()_+=-/*-'
-                break;
+      case 3:
+        characters = '0123456789'
+        break
 
-            default:
-                break;
-        }
+      case 4:
+        characters = '!@#$%^&*()_+=-/*-'
+        break
 
-        evaluatePassword()
-        
-        var charactersLength = characters.length;
-        passwordGenerator(characters, charactersLength)
+      default:
+        break
+    }
 
-    // IF NOT 
-    } else {
-        passwordResult.innerHTML = "escolhe alguma opcao batata!"
-    }   
+    evaluatePassword()
+
+    var charactersLength = characters.length
+    passwordGenerator(characters, charactersLength)
+
+  // IF NOT
+  } else {
+    passwordResult.innerHTML = '<div class="font-bold">Please choose at least one option!</div>'
+  }
 }
-
 
 function evaluatePassword() {
+  var strenghtName = document.getElementById('strenght')
+  var strenghtBgColor = document.querySelectorAll('.box')
 
-    var strenghtName = document.getElementById('strenght')
-    var strenghtBgColor = document.querySelectorAll('.box')
+  if (getCheckedBoxes().length > 3 && getRange() >= 8) {
+    strenghtName.innerHTML = 'VERY STORNG'
+    strenghtBgColor.forEach((element) => {
+      element.classList.add('bg-purple-700')
+    })
+  } else if (getCheckedBoxes().length >= 3 && getRange() >= 6) {
+    strenghtName.innerHTML = 'STRONG'
 
-    if(getCheckedBoxes().length > 3 && getRange() >= 8){
-        strenghtName.innerHTML = "VERY STORNG"
-        strenghtBgColor.forEach(element => {
-            element.classList.add('bg-yellow-500')
-        });
-
-    } else if (getCheckedBoxes().length >= 3 && getRange() >= 6){
-        strenghtName.innerHTML = "STRONG"
-
-        for (let i = 0; i < 3; i++) {
-            const element = strenghtBgColor[i];
-            element.classList.add('bg-yellow-500')
-        }
-        strenghtBgColor[3].classList.remove('bg-yellow-500')
-
-    } else if (getCheckedBoxes().length <= 2 && getRange() >= 5){
-        strenghtName.innerHTML = "MEDIUM"
-
-        for (let i = 0; i < 3; i++) {
-            const element = strenghtBgColor[i];
-            element.classList.add('bg-yellow-500')
-        }
-        strenghtBgColor[2].classList.remove('bg-yellow-500')
-
-    } else if (getCheckedBoxes().length <= 4 && getRange() <= 5 && getRange() >=4 ){
-        strenghtName.innerHTML = "WEAK"
-
-        for (let i = 0; i < 2; i++) {
-            const element = strenghtBgColor[i];
-            element.classList.add('bg-yellow-500')
-        }
-        strenghtBgColor[1].classList.remove('bg-yellow-500')
-
-    } else {
-        strenghtName.innerHTML = "VERY WEAK"
-        for (let i = 0; i < 4; i++) {
-            const element = strenghtBgColor[i];
-            element.classList.remove('bg-yellow-500')
-        }
+    for (let i = 0; i < 3; i++) {
+      const element = strenghtBgColor[i]
+      element.classList.add('bg-purple-700')
     }
+    strenghtBgColor[3].classList.remove('bg-purple-700')
+  } else if (getCheckedBoxes().length <= 2 && getRange() >= 5) {
+    strenghtName.innerHTML = 'MEDIUM'
+
+    for (let i = 0; i < 3; i++) {
+      const element = strenghtBgColor[i]
+      strenghtBgColor[3].classList.remove('bg-purple-700')
+      element.classList.add('bg-purple-700')
+    }
+    strenghtBgColor[2].classList.remove('bg-purple-700')
+  } else if (
+    getCheckedBoxes().length <= 4 &&
+    getRange() <= 5 &&
+    getRange() >= 4
+  ) {
+    strenghtName.innerHTML = 'WEAK'
+
+    for (let i = 0; i < 2; i++) {
+      const element = strenghtBgColor[i]
+      strenghtBgColor[2].classList.remove('bg-purple-700')
+      element.classList.add('bg-purple-700')
+    }
+    strenghtBgColor[1].classList.remove('bg-purple-700')
+  } else {
+    strenghtName.innerHTML = 'VERY WEAK'
+    for (let i = 0; i < 4; i++) {
+      const element = strenghtBgColor[i]
+      element.classList.remove('bg-purple-700')
+    }
+  }
 }
 
-function passwordGenerator(characters, charactersLength){
+function passwordGenerator(characters, charactersLength) {
+  var length = getRange()
+  var result = ''
 
-    var length = getRange()
-    var result = '';
-
-    for ( var i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    passwordResult.innerHTML = result;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength))
+  }
+  passwordResult.innerHTML = result
 }
 
 function getCheckedBoxes() {
-    
-    var checkedCheckBoxes = ''
-    const checkboxes = document.querySelectorAll('input[type=checkbox]:checked')
+  var checkedCheckBoxes = ''
+  const checkboxes = document.querySelectorAll('input[type=checkbox]:checked')
 
-    for (var i = 0; i < checkboxes.length; i++) {
-        checkedCheckBoxes += checkboxes[i].value
-    }
+  for (var i = 0; i < checkboxes.length; i++) {
+    checkedCheckBoxes += checkboxes[i].value
+  }
 
-    return checkedCheckBoxes
+  return checkedCheckBoxes
 }
-
-
